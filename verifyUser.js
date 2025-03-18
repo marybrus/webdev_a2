ea7563a36b699db9b14ccd8fd5a138df8f72aad4
Mary B <130492207+marybrus@users.noreply.github.com>
2025-03-18 13:51:39 +0000

    Update verifyUser.js

--- verifyUser.js
+++ verifyUser.js
@@ -8,11 +8,9 @@ function auth(req, res, next) {
     }
 
     try {
-        // Verify the token and get the decoded payload
         const verified = jsonwebtoken.verify(token, process.env.TOKEN_SECRET);
         req.user = verified; // Store the decoded data (e.g., user ID) in req.user
 
-        // Now we need to ensure that the user is the creator of the post they are trying to modify
         const postId = req.params.postId; // Extract the post ID from the URL parameter
 
         // Find the post by its ID
@@ -22,12 +20,10 @@ function auth(req, res, next) {
                     return res.status(404).send({ message: 'Post not found' });
                 }
 
-                // Compare the user ID in the JWT with the createdBy field of the post
                 if (post.createdBy.toString() !== req.user._id.toString()) {
                     return res.status(403).send({ message: 'You are not authorized to modify this post' });
                 }
 
-                // If the user is authorized, allow them to continue
                 next();
             })
             .catch(err => {
